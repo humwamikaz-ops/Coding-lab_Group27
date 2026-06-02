26aa09211bc85b5d36d0977fe4c2c1d010d697c5
NZIZA LAEL DAVID <d.nziza1@alustudent.com>
2026-06-02 14:11:53 +0200

    Update hospital_analysis.sh

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 process_vitals(){
-echo"scanning active logs for CRITICAL alerts..."
+echo "scanning active logs for CRITICAL alerts..."
 mkdir -p reports
 grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log 2>/dev/null | awk '{print $1, $2 ,$4}' >reports/critical_alerts.txt
 }
