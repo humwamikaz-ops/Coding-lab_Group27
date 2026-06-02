9d8fc1c9ca62666e7fecca24cb135522cac022b2
davidlael <d.nziza1@alustudent.com>
2026-06-02 04:18:02 -0700

    chore: continuous update to optimize hospital_analysis script

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -4,3 +4,4 @@ echo"scanning active logs for CRITICAL alerts..."
 mkdir -p reports
 grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log 2>/dev/null | awk '{print $1, $2 ,$4}' >reports/critical_alerts.txt
 }
+#continuous optimization updatefor Member 5 script
